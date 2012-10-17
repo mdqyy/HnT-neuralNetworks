5be4abd6b8a452b7c24f1b50ffa4957cfd602d68
Luc MIOULET <luc.mioulet@insa-rouen.fr>
2012-10-17 15:49:51 +0200

    adding file

---
+++ src/executables/CTCTesting.cpp
@@ -0,0 +1,75 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <iostream>
+#include <opencv/cv.h>
+#include <list>
+#include <vector>
+
+#include "../dataset/supervised/ClassificationDataset.hpp"
+#include "../dataset/ValueVector.hpp"
+#include "../dataset/Mask.hpp"
+#include "../dataset/FeatureVector.hpp"
+#include "../machines/neuralMachines/NeuralNetwork.hpp"
+#include "../machines/neuralMachines/layers/InputLayer.hpp"
+#include "../machines/neuralMachines/layers/LayerCTC.hpp"
+#include "../machines/neuralMachines/connections/Connection.hpp"
+#include "../trainer/supervisedTrainers/neuralNetworkTrainers/CTCTrainer.hpp"
+#include <sstream>
+
+using namespace std;
+using namespace cv;
+
+int main (int argc, char* argv[]){
+	ClassificationDataset dataset;
+	dataset.load(argv[1]);
+	cout << "dataset loaded, total elements : "<< dataset.getNumSamples()<< endl ;
+	int numClasses = dataset.getNumberOfClasses();
+	int numInputs = dataset[0][0].getLength();
+	ValueVector mean = dataset.getMean();
+	ValueVector stdDev = dataset.getStandardDeviation();
+	LayerCTC ctc = LayerCTC(numClasses,"ctc");
+	InputLayer input = InputLayer(numInputs,mean,stdDev,"input");
+//	Connection connection = Connection(&input,&ctc);
+	Mat weights = Mat(numInputs,numClasses,CV_64FC1);
+	Connection c2 = Connection(0,&ctc,weights);
+	c2.initializeWeights(1,0,1.0);
+	Mask mask;
+	CTCTrainer ctcTrainer = CTCTrainer(ctc, dataset,mask, mask);
+	ctcTrainer.train();
+	/*
+	params.setMaxIterations(iterations);
+	params.setLearningRate(0.001);
+
+	double t = (double)getTickCount();
+	pbp.train();
+	t = ((double)getTickCount() - t)/getTickFrequency();
+	cout << "Temps :" << t << endl;
+
+	cout << endl <<"Saving network" << endl;
+	ofstream outStream("IAMpop.txt");
+	outStream << pop;
+
+	vector<NeuralNetworkPtr> population= pop.getPopulation();
+	ClassificationDataset dataset2;
+	dataset2.load(argv[6]);
+	cout << "Recording Data" << endl;
+	for(uint i=0;i<population.size();i++){
+		ostringstream name,path;
+		name << "neuralNet" << i;
+		UnsupervisedDataset nnData;
+		nnData.setName(name.str());
+		for(uint j=0;j<dataset2.getNumSequences();j++){
+			vector<FeatureVector> features;
+			for(uint k=0;k<dataset2[j].size() ; k++){
+				population[i]->forward(dataset2[j][k]);
+				features.push_back(population[i]->getOutputSignal());
+			}
+			nnData.addSequence(features);
+		}
+		path<<filename<<name.str()<<".xml";
+		nnData.save(path.str());
+	}*/
+
+	return EXIT_SUCCESS;
+}
