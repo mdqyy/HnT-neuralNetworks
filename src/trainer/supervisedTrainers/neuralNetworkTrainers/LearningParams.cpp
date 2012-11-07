99054e6fe97f48fe35e2e6ed5b1e19038d30d727
Luc MIOULET <luc.mioulet@insa-rouen.fr>
2012-11-07 17:07:52 +0100

    added an executable to work on toy datasets

---
+++ src/trainer/supervisedTrainers/neuralNetworkTrainers/LearningParams.cpp
@@ -0,0 +1,191 @@
+/*!
+ * \file LearningParams.cpp
+ * Body of the LearningParams class.
+ * \author Luc Mioulet
+ */
+
+#include "LearningParams.hpp"
+
+using namespace cv;
+using namespace std;
+
+LearningParams::LearningParams(realv _learningRate, realv _learningRateDecrease, uint _maxIterations, uint _actualIteration, uint _maxTrained, realv _maxTrainedPercentage,
+		realv _errorToFirst, realv _errorToFirstIncrease, bool _savedDuringProcess, string _saveLocation, bool _validatedDuringProcess) :
+		learningRate(_learningRate), learningRateDecrease(_learningRateDecrease), maxIterations(_maxIterations), maxTrained(_maxTrained), maxTrainedPercentage(
+				_maxTrainedPercentage), errorToFirst(_errorToFirst), errorToFirstIncrease(_errorToFirstIncrease), savedDuringProcess(_savedDuringProcess), saveLocation(
+				_saveLocation), validatedDuringProcess(_validatedDuringProcess) {
+
+}
+
+realv LearningParams::getMinError(){
+  return minError;
+}
+
+realv LearningParams::getMinChangeError(){
+  return minChangeError;
+}
+
+realv LearningParams::getLearningRate() const {
+	return learningRate;
+}
+
+realv LearningParams::getLearningRateDecrease() const {
+	return learningRateDecrease;
+}
+
+uint LearningParams::getMaxIterations() const {
+	return maxIterations;
+}
+
+uint LearningParams::getMaxTrained() const {
+	return maxTrained;
+}
+
+realv LearningParams::getErrorToFirst() const {
+	return errorToFirst;
+}
+
+realv LearningParams::getErrorToFirstIncrease() const {
+	return errorToFirstIncrease;
+}
+
+void LearningParams::setLearningRate(realv _learningRate) {
+	learningRate = _learningRate;
+}
+
+void LearningParams::setLearningRateDecrease(realv _learningRateDecrease) {
+	learningRateDecrease = _learningRateDecrease;
+}
+
+void LearningParams::setMaxIterations(uint _maxIterations) {
+	maxIterations = _maxIterations;
+}
+
+void LearningParams::setMaxTrained(uint _maxTrained) {
+	maxTrained = _maxTrained;
+}
+
+void LearningParams::setErrorToFirst(realv _errorToFirst) {
+	if (_errorToFirst < 1.0) {
+		errorToFirst = _errorToFirst;
+	} else {
+		errorToFirst = 1.0;
+	}
+}
+
+void LearningParams::setErrorToFirstIncrease(realv _errorToFirstIncrease) {
+	errorToFirstIncrease = _errorToFirstIncrease;
+}
+
+realv LearningParams::getMaxTrainedPercentage() const {
+	return maxTrainedPercentage;
+}
+
+void LearningParams::setMaxTrainedPercentage(realv maxTrainedPercentage) {
+	if (maxTrainedPercentage <= 1.0 && maxTrainedPercentage >= 0.0) {
+		this->maxTrainedPercentage = maxTrainedPercentage;
+	}
+}
+
+bool LearningParams::isSavedDuringProcess() const {
+	return savedDuringProcess;
+}
+
+void LearningParams::setSavedDuringProcess(bool _saveDuringProcess) {
+	this->savedDuringProcess = _saveDuringProcess;
+}
+
+std::string LearningParams::getSaveLocation() const {
+	return saveLocation;
+}
+
+void LearningParams::setSaveLocation(std::string _saveLocation) {
+	this->saveLocation = _saveLocation;
+}
+
+uint LearningParams::getActualIteration() const {
+	return actualIteration;
+}
+
+void LearningParams::setActualIteration(uint _actualIteration) {
+	this->actualIteration = _actualIteration;
+}
+
+bool LearningParams::isValidatedDuringProcess() const {
+	return validatedDuringProcess;
+}
+
+void LearningParams::setValidatedDuringProcess(bool _validatedDuringProcess) {
+	this->validatedDuringProcess = _validatedDuringProcess;
+}
+
+void LearningParams::setMinChangeError(realv _minChangeError) {
+	this->minChangeError = _minChangeError;
+}
+
+void LearningParams::setMinError(realv _minError) {
+	this->minError = _minError;
+}
+
+bool LearningParams::isStochastic() const {
+	return stochastic;
+}
+
+int LearningParams::getTask() const {
+	return task;
+}
+
+void LearningParams::setTask(int task) {
+	this->task = task;
+}
+
+void LearningParams::setStochastic(bool _stochastic) {
+	this->stochastic = _stochastic;
+}
+
+LearningParams::~LearningParams() {
+
+}
+
+ofstream& operator<<(ofstream& _ofs, const LearningParams& _p) {
+	_ofs << "< ";
+	_ofs << " " << _p.getErrorToFirst();
+	_ofs << " " << _p.getErrorToFirstIncrease();
+	_ofs << " " << _p.getLearningRate();
+	_ofs << " " << _p.getLearningRateDecrease();
+	_ofs << " " << _p.getMaxIterations();
+	_ofs << " " << _p.getActualIteration();
+	_ofs << " " << _p.getMaxTrainedPercentage();
+	_ofs << " " << _p.isSavedDuringProcess();
+	_ofs << " " << _p.getSaveLocation();
+	_ofs << " >";
+	return _ofs;
+}
+
+ifstream& operator>>(ifstream& _ifs, LearningParams& _p) {
+	string stringValues;
+	realv realValues;
+	int intValues;
+	bool boolValues;
+	_ifs >> stringValues;
+	_ifs >> realValues;
+	_p.setErrorToFirst(realValues);
+	_ifs >> realValues;
+	_p.setErrorToFirstIncrease(realValues);
+	_ifs >> realValues;
+	_p.setLearningRate(realValues);
+	_ifs >> realValues;
+	_p.setLearningRateDecrease(realValues);
+	_ifs >> intValues;
+	_p.setMaxIterations(intValues);
+	_ifs >> intValues;
+	_p.setActualIteration(intValues);
+	_ifs >> realValues;
+	_p.setMaxTrainedPercentage(realValues);
+	_ifs >> boolValues;
+	_p.setSavedDuringProcess(boolValues);
+	_ifs >> stringValues;
+	_p.setSaveLocation(stringValues);
+	_ifs >> stringValues;
+	return _ifs;
+}
---
+++ src/trainer/supervisedTrainers/neuralNetworkTrainers/LearningParams.hpp
@@ -0,0 +1,292 @@
+#ifndef __LEARNINGPARAMS_HPP__
+#define __LEARNINGPARAMS_HPP__
+/*!
+ * \file LearningParams.hpp
+ * Header of the LearningParams class.
+ * \author Luc Mioulet
+ */
+
+#include "../../../General.hpp"
+#include <opencv/cv.h>
+#include <stdio.h>
+#include <fstream>
+#include <ostream>
+#include <istream>
+#include <sstream>
+
+#define BP_CLASSIFICATION 0
+#define BP_REGRESSION 1
+#define BP_AUTOENCODER 2
+
+/*!
+ * \class LearningParams
+ * Learning parameters for neural networks.
+ */
+class LearningParams {
+private:
+
+protected:
+	/*! Do stochastic learning (randomize sequence selection).*/
+	bool stochastic;
+
+	/*! Learning rate for weight change. */
+	realv learningRate;
+
+	/*! Learning rate decrease between each step. */
+	realv learningRateDecrease;
+
+	/*! Maximum number of iterations */
+	uint maxIterations;
+
+	/*! actualIteration */
+	uint actualIteration;
+
+	/*! Minimum error change between two steps*/
+	realv minError;
+
+	/*! Minimum error change between two iterations */
+	realv minChangeError;
+
+	/*! Maximum number of networks to train per iteration*/
+	uint maxTrained;
+
+	/*! Maximum percentage of trained samples (range from 0 to 1) */
+	realv maxTrainedPercentage;
+
+	/*! Error ratio between best classifier and worst selected for training.*/
+	realv errorToFirst;
+
+	/*! Error ratio increase */
+	realv errorToFirstIncrease;
+
+	/*! Save during process */
+	bool savedDuringProcess;
+
+	/*! Save location */
+	std::string saveLocation;
+
+	/*! Validate during process */
+	bool validatedDuringProcess;
+
+	/*! Problem type (BP_CLASSIFICATION, BP_REGRESSION, BP_AUTOENCODER) */
+	int task;
+
+public:
+
+	/*!
+	 * Default constructor.
+	 */
+	LearningParams(realv _learningRate = 0.001, realv _learningRateDecrease = 0.95, uint _maxIterations = 10, uint _actualIteration = 0, uint _maxTrained = 5,
+			realv _maxTrainedPercentage = 0.25, realv _errorToFirst = 0.5, realv errorToFirstIncrease = 1.1, bool _savedDuringProcess = false,
+			std::string _saveLocation = ".", bool _validatedDuringProcess = true);
+
+	/*!
+	 * Do stochastic.
+	 * \return Do stochastic.
+	 */
+	bool getDoStochastic();
+
+	/*!
+	 * Get the minimum error to be achieved.
+	 * \return Minimum error to be achieved.
+	 */
+	realv getMinError();
+
+	/*!
+	 * Get minimum error change for which the backpropagation shoud be stopped.
+	 * \return Minimum error change.
+	 */
+	realv getMinChangeError();
+
+	/*!
+	 * Get learning rate.
+	 * \return Weight change rate per iteration.
+	 */
+	realv getLearningRate() const;
+
+	/*!
+	 * Get Learning rate decrease.
+	 * \return Weight decrease.
+	 */
+	realv getLearningRateDecrease() const;
+
+	/*!
+	 * Get maximum number of iterations.
+	 * \return Maximum number of iterations.
+	 */
+	uint getMaxIterations() const;
+
+	/*!
+	 * Get the maxmimum number of networks to train at each iteration.
+	 * \return Maximum of netwoks trained.
+	 */
+	uint getMaxTrained() const;
+
+	/*!
+	 * Get error to best network selected.
+	 * \return Error to best network.
+	 */
+	realv getErrorToFirst() const;
+
+	/*!
+	 * Get ratio increase to best network selected.
+	 * \return Increase ratio to best network.
+	 */
+	realv getErrorToFirstIncrease() const;
+
+	/*!
+	 * Set learning rate.
+	 * \param _learningRate New learning rate.
+	 */
+	void setLearningRate(realv _learningRate);
+
+	/*!
+	 * Set learning rate decrease.
+	 * \param _learningRateDecrease Learing rate decrease.
+	 */
+	void setLearningRateDecrease(realv _learningRateDecrease);
+
+	/*!
+	 * Set maximum iterations.
+	 * \param _maxIterations Maximum number of iterations.
+	 */
+	void setMaxIterations(uint _maxIterations);
+
+	/*!
+	 * Set maximum of networks to train.
+	 * \param _maxTrained Maximum of networks to train.
+	 */
+	void setMaxTrained(uint _maxTrained);
+
+	/*!
+	 * Set the error to first ratio.
+	 * \param _errorToFirst Error to first.
+	 */
+	void setErrorToFirst(realv _errorToFirst);
+
+	/*!
+	 * Set the increase for the ratio to first.
+	 * \param _errorToFirstIncrease Increase.
+	 */
+	void setErrorToFirstIncrease(realv _errorToFirstIncrease);
+
+	/*!
+	 * Get maximum percentage of trained samples over one iteration.
+	 */
+	realv getMaxTrainedPercentage() const;
+
+	/*!
+	 * Set maximum percentage of trained samples over one iteration.
+	 */
+	void setMaxTrainedPercentage(realv _maxTrainedPercentage);
+
+	/*!
+	 * Get saved during process.
+	 * \return Is saved during learning.
+	 */
+	bool isSavedDuringProcess() const;
+
+	/*!
+	 * Set saved during process.
+	 * \param Is saved during learning.
+	 */
+	void setSavedDuringProcess(bool _savedDuringProcess);
+
+	/*!
+	 * Get save location.
+	 * \return Save location.
+	 */
+	std::string getSaveLocation() const;
+
+	/*!
+	 * Set save location
+	 * \param _saveLocation Save location.
+	 */
+	void setSaveLocation(std::string _saveLocation);
+
+	/*!
+	 * Get actual iteration.
+	 * \return Actual iteration.
+	 */
+	uint getActualIteration() const;
+
+	/*!
+	 * Set actual iteration.
+	 * \param _actualIteration Actual iteration.
+	 */
+	void setActualIteration(uint _actualIteration);
+
+	/*!
+	 * Get validated during process.
+	 * \return Is validated during learning.
+	 */
+	bool isValidatedDuringProcess() const;
+
+	/*!
+	 * Set validated during process.
+	 * \param Is validated during learning.
+	 */
+	void setValidatedDuringProcess(bool _validatedDuringProcess);
+
+	/*!
+	 * Set the minimum change error.
+	 * \param _minChangeError Minimum change in error before stopping learning.
+	 */
+	void setMinChangeError(realv _minChangeError);
+
+	/*!
+	 * Set the minimum error.
+	 * \param _minError Minimum error to achieve before stopping learning.
+	 */
+	void setMinError(realv _minError);
+
+	/*!
+	 * Get if the process is set as stochastic (random selection of a subpart of the dataset).
+	 * \return Is stochastic.
+	 */
+	bool isStochastic() const;
+
+	/*!
+	 * Set the learning to be stochastic. Also change the maximum percentage of trained samples.
+	 * \param _stochastic True or false.
+	 */
+	void setStochastic(bool _stochastic);
+
+	/*!
+	 * Get the task.
+	 * \return Get the processing task.
+	 */
+	int getTask() const;
+
+	/*!
+	 * Set the task.
+	 * \param _task The learning task.
+	 */
+	void setTask(int _task);
+
+	/*!
+	 * Destructor.
+	 */
+	~LearningParams();
+
+	/*!
+	 * Output file stream.
+	 * \param _ofs Output file stream.
+	 * \param _p Population Backprop networks.
+	 * \return Output file stream.
+	 */
+	friend std::ofstream& operator<<(std::ofstream& _ofs, const LearningParams& _p);
+
+	/*!
+	 * Input file stream.
+	 * \param _ifs Input file stream.
+	 * \param _p Population Backprop networks.
+	 * \return Input file stream.
+	 */
+	friend std::ifstream& operator>>(std::ifstream& _ifs, LearningParams& _p);
+
+
+}
+;
+
+#endif
